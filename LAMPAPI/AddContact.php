ed50e51efb631770c80ffae749799ef1352439a8
DigiOceanServer <digital.oceans.server@gmail.com>
2021-01-15 23:57:38 +0000

    Mark: Contacts and Search Contacts

---
+++ LAMPAPI/AddContact.php
@@ -0,0 +1,44 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$phoneNumber = $inData["phoneNumber"];
+	$address = $inData["address"];
+	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$sql = "insert into Contacts (UserId,FirstName,LastName,PhoneNumber,Address) VALUES (" . $userId . ",'" . $firstName . ",'" . $lastName . ",'" . $phoneNumber .  ",'" .  $address . "')";
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+	
+	returnWithError("");
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
