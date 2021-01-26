811b315e12c919c2ebbb77e05be8da447708f043
Tyler Hostler-Mathis <Hostler.Mathis.Tyler@gmail.com>
2021-01-25 20:09:38 -0500

    Quotes -.-

--- js/user.js
+++ js/user.js
@@ -4,7 +4,7 @@ const extension = '.php';
 // Tries to autologin a user. If it can't then it loads the hi res background
 window.onload = function () {
 	const dom = "https://contactical.xyz";
-	if ((window.location.href == dom.concat(index.html) ||
+	if ((window.location.href == dom.concat("index.html") ||
 		window.location.href == dom) && autoLogin())
 		return;
 	loadHiRes();
