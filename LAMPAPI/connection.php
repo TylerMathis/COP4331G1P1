50348ed081763da79dc1e4b90b23e59b455f526c
beedude <suneettipirneni@icloud.com>
2021-01-31 22:15:02 -0500

    Add documentation

--- LAMPAPI/connection.php
+++ LAMPAPI/connection.php
@@ -2,6 +2,27 @@
 include_once 'secret.php';
 include_once 'Database.php';
 
+/*
+ * You should define the credentials for the SQL server,
+ * secret.php should be created and should fit the needs of your
+ * local SQL setup. It will automatically be gitignored.
+*/
+
+/* Place in secret.php:
+
+// Database Hostname
+define("SQL_HOST", "127.0.0.1");
+
+// Database Username
+define("SQL_LOGIN", "username");
+
+// Database Password
+define("SQL_PASS", "password");
+
+// Database Schema
+define("SQL_DB", "COP4331");
+*/
+
 if (!defined("SQL_LOGIN") || !defined("SQL_PASS") || !defined("SQL_HOST") || !defined("SQL_DB")) {
     echo "Please define SQL credentials in the secret.php file!";
     die(1);
