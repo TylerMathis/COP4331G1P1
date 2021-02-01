4e118ca2375264e45524daf8c049acafc6fecf87
beedude <suneettipirneni@icloud.com>
2021-01-31 22:01:17 -0500

    Add secret template and constant checks

--- LAMPAPI/connection.php
+++ LAMPAPI/connection.php
@@ -1,5 +1,11 @@
 <?php
+include_once 'secret.php';
 include_once 'Database.php';
 
+if (!defined("SQL_LOGIN") || !defined("SQL_PASS") || !defined("SQL_HOST") || !defined("SQL_DB")) {
+    echo "Please define SQL credentials in the secret.php file!";
+    die(1);
+}
+
 // Create persistent instance of database
-$db = new Database("TheBeast", "ProwlsTheServer", "127.0.0.1", "COP4331");
+$db = new Database(SQL_LOGIN, SQL_PASS, SQL_HOST, SQL_DB);
---
+++ LAMPAPI/secret.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Defines the credentials for the SQL server, this file
+ * should be gitignored and should fit the needs of your
+ * local SQL setup.
+*/
+
+/*
+// Database Hostname
+define("SQL_HOST", "127.0.0.1");
+
+// Database Username
+define("SQL_LOGIN", "username");
+
+// Database Password
+define("SQL_PASS", "password");
+
+// Database Schema
+define("SQL_DB", "COP4331");
+*/
