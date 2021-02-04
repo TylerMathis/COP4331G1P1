01dca30c1948220d383d371e86ac06c868561186
beedude <suneettipirneni@icloud.com>
2021-02-03 19:33:04 -0500

    Cleanup CSS

--- js/landing_page.js
+++ js/landing_page.js
@@ -3,4 +3,14 @@ window.onload = function () {
     populateUserCache();
     welcomeUser();
     populateContacts(true);
+    populateModals();
 };
+
+/**
+ * Populates the modal html in on load so it doesn't clutter the base html.
+ */
+function populateModals() {
+    $("#edit-modal-prop").load("../modal/editModal.html");
+    $("#delete-modal-prop").load("../modal/deleteModal.html");
+    $("#new-modal-prop").load("../modal/createModal.html");
+}
