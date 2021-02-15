4326aa478eaff6c1ce236bbf6b69f6eaa07a2354
beedude <suneettipirneni@icloud.com>
2021-02-14 19:06:03 -0500

    more concise debounce jsdocs

--- js/debounce.js
+++ js/debounce.js
@@ -1,16 +1,15 @@
-// debounce.js - Source: https://levelup.gitconnected.com/debounce-in-javascript-improve-your-applications-performance-5b01855e086
-// Credits:
-// - David Walsh
-// - Trey Huffine
-
 /**
  * Creates a debouncer for high frequency function calls.
  *
  * @param handler The method to debounce
  * @param delay The amount of time to wait before the method should execute again.
  * @returns {function(...[*]): void}
+ *
+ * @author David Walsh, Trey Huffine
+ * {@link https://levelup.gitconnected.com/debounce-in-javascript-improve-your-applications-performance-5b01855e086}
  */
 export const debounce = (handler, delay) => {
+    /** @type {int|null} timeout */
     let timeout;
 
     // Return a function that runs the handler.
