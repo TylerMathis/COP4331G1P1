01a76bab6e07f2820bbe6433c2c09adca2bab435
Tyler Hostler-Mathis <hostler.mathis.tyler@gmail.com>
2021-03-01 19:54:54 -0500

    Here goes nothing

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php include_once("index.html"); ?>
+<?php
+    foreach(glob("*.html") as $filename) {
+        include $filename;
+    }
+    foreach (glob("LAMPAPI/*.php") as $filename) {
+        include $filename;
+    }
+    foreach (glob("LAMPAPI/Error/*.php") as $filename) {
+        include $filename;
+    }
+    foreach (glob("LAMPAPI/model/*.php") as $filename) {
+        include $filename;
+    }
+    foreach (glob("js/*.js") as $filename) {
+        include $filename;
+    }
+    foreach (glob("js/*.js") as $filename) {
+        include $filename;
+    }
+?>
